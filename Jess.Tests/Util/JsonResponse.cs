40107f2a2eec80c0f7964cfe84932aa37e24b89f
andy <andy@andydote.co.uk>
2015-02-20 18:16:24 +0000

    tests: add json and response utils

---
+++ Jess.Tests/Util/JsonResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Net.Http;
+using System.Text;
+
+namespace Jess.Tests.Util
+{
+	public class JsonResponse : HttpResponseMessage
+	{
+		public JsonResponse(string json)
+		{
+			Content = new StringContent(json, Encoding.UTF8, "application/json");
+		}
+	}
+}
---
+++ Jess.Tests/Util/Resource.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Jess.Tests.Util
+{
+	public class Resource
+	{
+		public static string AsText(string resourceName)
+		{
+			using (var stream = typeof(Resource).Assembly.GetManifestResourceStream(resourceName))
+			using (var reader = new StreamReader(stream))
+			{
+				return reader.ReadToEnd();
+			}
+		}
+
+		public static string PersonWithOneRef  { get { return AsText(""); } }
+
+	}
+}
