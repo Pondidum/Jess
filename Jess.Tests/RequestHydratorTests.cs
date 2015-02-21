d1ebfc2771c697e3098d81b85dd8ab2a9b32fad8
andy <andy@andydote.co.uk>
2015-02-21 14:40:09 +0000

    requestHydrator: copy first stream to second, line by line

---
+++ Jess.Tests/RequestHydratorTests.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using Jess.Tests.Util;
+using Shouldly;
+using Xunit;
+
+namespace Jess.Tests
+{
+	public class RequestHydratorTests
+	{
+		[Fact]
+		public void An_input_with_no_replacements_is_unmodified()
+		{
+			using (var input = StreamFrom(Resource.PersonWithOneRefHydrated))
+			using (var output = new MemoryStream())
+			{
+				var hydrator = new ResponseHydrator();
+				hydrator.Hydrate(input, output);
+
+				output.Position = 0;
+
+				StringFrom(output).ShouldBe(Resource.PersonWithOneRefHydrated);
+			}
+		}
+
+		private MemoryStream StreamFrom(string input)
+		{
+			var ms = new MemoryStream();
+			var writer = new StreamWriter(ms);
+			writer.Write(input);
+			writer.Flush();
+
+			ms.Position = 0;
+			return ms;
+		}
+
+		private string StringFrom(Stream input)
+		{
+			using (var reader = new StreamReader(input))
+			{
+				return reader.ReadToEnd();
+			}
+		}
+	}
+}
---
+++ Jess/RequestHydrator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Jess
+{
+	public class ResponseHydrator
+	{
+		public void Hydrate(Stream input, Stream output)
+		{
+			using (var inputReader = new StreamReader(input))
+			{
+				string line;
+				while ((line = inputReader.ReadLine()) != null)
+				{
+					var bytes = Encoding.UTF8.GetBytes(line + Environment.NewLine);
+					output.Write(bytes, 0, bytes.Length);
+					output.Flush();
+				}
+			}
+		}
+
+	}
+}
