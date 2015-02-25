fadd050b0665ba2bb2f8b22fd8fc744764c51484
andy <andy@andydote.co.uk>
2015-02-25 17:15:45 +0000

    tests: add multiple ref resources

--- Jess.Tests/Util/Resource.cs
+++ Jess.Tests/Util/Resource.cs
@@ -16,5 +16,9 @@ namespace Jess.Tests.Util
 
 		public static string PersonWithOneRef { get { return AsText("Jess.Tests.Data.person-with-one-ref.json"); } }
 		public static string PersonWithOneRefHydrated { get { return AsText("Jess.Tests.Data.person-with-one-ref-hydrated.json"); } }
+
+		public static string PersonWithMutlipleRefs { get { return AsText("Jess.Tests.Data.person-with-multiple-refs.json"); } }
+		public static string PersonWithMutlipleRefsHydrated { get { return AsText("Jess.Tests.Data.person-with-multiple-refs-hydrated.json"); } }
+
 	}
 }
