fd0da04790d88d7a253ccd9e9b4f6aba504b86a1
andy <andy@andydote.co.uk>
2015-02-28 12:03:08 +0000

    requestHydrator: hide the Reference class instide RequestHydrator

--- Jess/Reference.cs
+++
@@ -1,8 +0,0 @@
-namespace Jess
-{
-	public class Reference
-	{
-		public string ID { get; set; }
-		public string Type { get; set; }
-	}
-}
--- Jess/RequestHydrator.cs
+++ Jess/RequestHydrator.cs
@@ -71,5 +71,11 @@ namespace Jess
 		{
 			return json.TrimStart(' ', '{').TrimEnd(' ', '}');
 		}
+
+		private class Reference
+		{
+			public string ID { get; set; }
+			public string Type { get; set; }
+		}
 	}
 }
